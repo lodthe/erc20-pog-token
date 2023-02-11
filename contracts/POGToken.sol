23600392bf0665c83f65e192d745dd69fcb03e02
Igor Baliuk <baliuk.igor@gmail.com>
2023-02-11 16:18:35 +0300

    Add goerli network via alchemy

--- contracts/POGToken.sol
+++ contracts/POGToken.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract POGToken is Ownable, ERC20 {
-    constructor(uint256 initialSupply) public ERC20("PogChamp Token", "POG") {
+    constructor(uint256 initialSupply) ERC20("PogChamp Token", "POG") {
         _mint(msg.sender, initialSupply);
     }
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,13 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
 };
 
 export default config;
